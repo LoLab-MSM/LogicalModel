87a03c7caf64a0b2f295b4af2811ab1cd19c0d63
Leonard A. Harris <lh64@cornell.edu>
2014-12-03 18:36:27 -0600

    Removing setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
- import numpy
-from Cython.Build import cythonize
-setup(
-    ext_modules = cythonize('func_example.pyx',include_path = [numpy.get_include()])
-    )
