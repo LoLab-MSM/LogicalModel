25206834b66487fa86ef8fa8333f1ca682dcb1ec
jamespino <james.c.pino@vanderbilt.edu>
2015-04-10 00:56:18 -0500

    adding file that was ignored

---
+++ changeBase.pyx
@@ -0,0 +1,25 @@
+from __future__ import division
+import numpy as np
+cimport numpy as np    
+cimport cython    
+
+DTYPE = np.int  
+ctypedef np.int_t DTYPE_t 
+def changebase(number,numNodes,numStates):
+    cdef int counter = -1
+    cdef int Number = number
+    cdef int NumNodes = numNodes
+    cdef int NumStates = numStates
+    cdef np.ndarray[DTYPE_t, ndim=1] state = np.zeros([numNodes],dtype=int)
+    cdef int quotient = np.int(Number/NumStates)
+    cdef int remainder = Number % NumStates
+    Number = quotient
+    state[counter] = remainder
+    counter -=1
+    while quotient !=0:
+        quotient = np.int(Number/NumStates)
+        remainder = Number % NumStates
+        state[counter] = remainder
+        counter -=1
+        Number = quotient
+    return state
